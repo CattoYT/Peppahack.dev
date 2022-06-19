65bb1b46ffac3d537057c87baef96f830090c8ed
CattoYT <97292582+CattoYT@users.noreply.github.com>
2022-06-19 11:37:02 +0100

    Fake Login

--- Astra/astraScript.js
+++ Astra/astraScript.js
@@ -4,3 +4,12 @@ function download(fileUrl, fileName) {
     a.setAttribute("download", fileName);
     a.click();
   }
+
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if (username == "Catto" && password == "DeveloperBuild1") {
+      alert("Login Successful, Welcome back Catto. This also does nothing xd");
+    }
+}
